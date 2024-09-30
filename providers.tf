768a8d7ab4dca39e23269b943f09584ec9913304
mrolmedo <96732346+mrolmedo@users.noreply.github.com>
2024-09-30 14:05:07 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,12 +4,13 @@ terraform {
 
     rancher2 = {
       source  = "rancher/rancher2"
-      version = "3.1.1"
+      version = "5.0.0"
     }
   } # End of required_providers
 }   # End of terraform
 
 provider "rancher2" {
   api_url   = file("${path.cwd}/files/.rancher-api-url")
+  insecure  = true
   token_key = file("${path.cwd}/files/.rancher-bearer-token")
 }
